3bea37f87eace5a75add76291af1d2c4f46c02b0
Derek Zuk <derekzuk@gmail.com>
2019-05-12 12:08:59 -0400

    Initial Commit

---
+++ swift-mobile-wallet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swift-mobile-wallet
+//
+//  Created by Derek Zuk on 5/12/19.
+//  Copyright © 2019 Derek Zuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
