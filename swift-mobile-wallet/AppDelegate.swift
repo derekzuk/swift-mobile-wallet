d47b8518dbf3861f6921f0e7d187fb6b47057917
Derek Zuk <derekzuk@gmail.com>
2019-06-20 22:13:46 -0400

    Using color pallete

--- swift-mobile-wallet/AppDelegate.swift
+++ swift-mobile-wallet/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         UINavigationBar.appearance().tintColor = UIColor.black
-        UINavigationBar.appearance().barTintColor = UIColor.green
+        UINavigationBar.appearance().barTintColor = UIColor(named: "darkGreen")
         return true
     }
 
