518f6777ccca7721db974915b4d61029f967a86a
Derek Zuk <derekzuk@gmail.com>
2019-06-27 19:07:44 -0400

    Adding SettingsViewController

---
+++ swift-mobile-wallet/Settings/SettingsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SettingsViewController.swift
+//  swift-mobile-wallet
+//
+//  Created by Derek Zuk on 6/27/19.
+//  Copyright © 2019 Derek Zuk. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
